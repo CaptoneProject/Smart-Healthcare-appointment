79fa01c9b63e5154af649afaee9af58dc3372094
CaptoneProject <malavikarajanala9@gmail.com>
2025-03-03 21:57:10 -0500

    Update deploy.sh

--- medical/deploy.sh
+++ medical/deploy.sh
@@ -3,13 +3,16 @@ set -e
 
 echo "🚀 Deploying Smart Healthcare Vite App from Jenkins"
 
-cd medical 
+cd medical
 
 npm install
 npm run build
 
 pm2 delete healthcare-app || true
-pm2 start "serve -s dist -l 3000" --name healthcare-app
+
+# Correct way to pass arguments to `serve` via PM2
+pm2 start serve --name healthcare-app -- -s dist -l 3000
+
 pm2 save
 
 echo "✅ App deployed and running at: http://18.225.149.38:3000"
